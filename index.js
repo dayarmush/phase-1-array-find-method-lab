ec98782c018834fa0983c294f3d458248f31470d
dovid yarmush <yarmushda@gmail.com>
2023-05-04 14:55:40 -0400

    completed find lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+function superbowlWin(records) {
+    const result = records.find((record) => {
+        return record.result === 'W';
+    })
+    if (result === undefined) {
+        return undefined
+    } else {
+        return result.year
+    }
+}
+
